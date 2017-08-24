1266b69def1cfe49412b2baf886e136fd29dffea
Kaisma Penn-Titley <kptitley@gmail.com>
2017-08-24 12:46:35 -0400

    updated js code to reset form fields upon pressing reset fields button

--- apptform.js
+++ apptform.js
@@ -1,18 +1,19 @@
-var firstName = document.getElementById("fname"),
-// const lastName = document.getElementById("lname"),
-// const address = document.getElementById("addr"),
-// const telephone = document.getElementById("phone"),
-// const email = document.getElementById("email"),
-// const apptDate = document.getElementById("appt-date"),
-// const apptTime = document.getElementById("appt-time"),
-var resetBtn = document.getElementById("resetForm");
+const firstName = document.getElementById("fname"),
+lastName = document.getElementById("lname"),
+address = document.getElementById("addr"),
+telephone = document.getElementById("phone"),
+email = document.getElementById("email"),
+apptDate = document.getElementById("appt-date"),
+apptTime = document.getElementById("appt-time"),
+resetBtn = document.getElementById("resetForm");
 
 resetBtn.addEventListener("click", function() {
-    fname.value = "";
-    // lname.value = "";
-    // // addr.value = "";
-    // // phone.value = "";
-    // // email.value = "";
-    // // appt-date.value = "";
-    alert(fname.value);
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("addr").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("appt-date").value = "";
+    document.getElementById("appt-time").value = "";
+
 });
