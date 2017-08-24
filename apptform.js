67ebaa4db20f58901501452ee10f350172ec4056
Kaisma Penn-Titley <kptitley@gmail.com>
2017-08-24 12:23:55 -0400

    styled html with corresponing CSS including Bootstrap and Jquery CDN links

--- apptform.js
+++ apptform.js
@@ -1,14 +1,18 @@
-const firstName = document.getElementById("fname");
+var firstName = document.getElementById("fname"),
 // const lastName = document.getElementById("lname"),
-// const firstName = document.getElementById("fname"),
-// const firstName = document.getElementById("fname"),
-// const firstName = document.getElementById("fname"),
-// const firstName = document.getElementById("fname"),
-// const firstName = document.getElementById("fname"),
-// const firstName = document.getElementById("fname"),
-const resetBtn = document.getElementById("resetForm");
+// const address = document.getElementById("addr"),
+// const telephone = document.getElementById("phone"),
+// const email = document.getElementById("email"),
+// const apptDate = document.getElementById("appt-date"),
+// const apptTime = document.getElementById("appt-time"),
+var resetBtn = document.getElementById("resetForm");
 
 resetBtn.addEventListener("click", function() {
     fname.value = "";
-    
+    // lname.value = "";
+    // // addr.value = "";
+    // // phone.value = "";
+    // // email.value = "";
+    // // appt-date.value = "";
+    alert(fname.value);
 });
