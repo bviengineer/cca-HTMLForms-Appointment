c6e41dd31c0a69fc7f670f0926bd8c2fc9e87e68
Kaisma Penn-Titley <kptitley@gmail.com>
2017-08-24 10:59:09 -0400

    added a rest button to form and added a javascript file to handle what reset button does

---
+++ apptform.js
@@ -0,0 +1,14 @@
+const firstName = document.getElementById("fname");
+// const lastName = document.getElementById("lname"),
+// const firstName = document.getElementById("fname"),
+// const firstName = document.getElementById("fname"),
+// const firstName = document.getElementById("fname"),
+// const firstName = document.getElementById("fname"),
+// const firstName = document.getElementById("fname"),
+// const firstName = document.getElementById("fname"),
+const resetBtn = document.getElementById("resetForm");
+
+resetBtn.addEventListener("click", function() {
+    fname.value = "";
+    
+});
